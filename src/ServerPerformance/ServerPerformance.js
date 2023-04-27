214611143a9df3da98aa2db3d60b3e1ff8cf39f1
Amit Rai <8616994+raiamitkumar@users.noreply.github.com>
2023-04-27 12:01:52 +0000

    Added react router

---
+++ src/ServerPerformance/ServerPerformance.js
@@ -0,0 +1,25 @@
+import logo from '../logo.svg';
+import '../App.css';
+
+function ServerPerformance() {
+  return (
+    <div className="server-performance-container">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ServerPerformance from './ServerPerformance/ServerPerformance';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="serverperf" element={<ServerPerformance />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
